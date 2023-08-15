170f0b68e8b7da6b91cfb64af873b9150b60d5cb
newsks <sweetysks23@gmail.com>
2023-08-15 12:40:34 +0900

    README.md 업데이트

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
-// import
-const path = require('path')   // 전역모듈
+// path: NodeJS에서 파일 및 디렉토리 경로 작업을 위한 전역 모듈
+const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
-// export
 module.exports = {
-  // parcel index.html
   // 파일을 읽어들이기 시작하는 진입점 설정
-  entry:'./js/main.js',
+  entry: './js/main.js',
 
   // 결과물(번들)을 반환하는 설정
-  output:{
-    // path: path.resolve(__dirname,'dist'),
-    // filename:'main.js',
+  output: {
+    // 주석은 기본값!, `__dirname`은 현재 파일의 위치를 알려주는 NodeJS 전역 변수
+    // path: path.resolve(__dirname, 'dist'),
+    // filename: 'main.js',
     clean: true
   },
 
+  // 모듈 처리 방식을 설정
   module: {
     rules: [
       {
-        test:/\.s?css$/,
+        test: /\.s?css$/,
         use: [
+          // 순서 중요!
           'style-loader',
           'css-loader',
           'postcss-loader',
@@ -28,7 +29,8 @@ module.exports = {
         ]
       },
       {
-        test:/\.js$/,
+        test: /\.js$/,
+        exclude: /node_modules/, // 제외할 경로
         use: [
           'babel-loader'
         ]
@@ -39,7 +41,7 @@ module.exports = {
   // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
   plugins: [
     new HtmlPlugin({
-      template:'./index.html'
+      template: './index.html',
     }),
     new CopyPlugin({
       patterns: [
@@ -48,7 +50,10 @@ module.exports = {
     })
   ],
 
-  // devServer: {
-  //   host:'localhost'
-  // }
+  // 개발 서버 옵션
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    hot: true
+  }
 }
